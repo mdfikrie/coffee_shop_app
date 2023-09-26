0ff4dea5327baf0b204a4ec02e0bd980b085f01c
mdfikrie <mdfikrikh@gmail.com>
2023-09-26 15:17:01 +0700

    first commit

---
+++ Coffee_shop_app/Coffee_shop_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cofee_shop_appApp.swift
+//  Cofee_shop_app
+//
+//  Created by user on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_shop_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coffee_shop_app/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cofee_shop_app
+//
+//  Created by user on 26/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
